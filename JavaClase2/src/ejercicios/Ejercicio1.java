49118dac84d31147bef49d0ddaf6c80538c6457e
Samuel <Samuel>
2024-04-02 14:49:03 -0500

    commit

--- JavaClase2/src/ejercicios/Ejercicio1.java
+++ JavaClase2/src/ejercicios/Ejercicio1.java
@@ -9,14 +9,25 @@ import java.util.Scanner;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// Se crea el objeto scanner que permite el ingreso de datos a través de la consola
-		Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in); // Permite el ingreso de datos a través de la consola
 		
-		System.out.print("Ingrese un número real: ");
+		System.out.print("Ingrese un número real (x): ");
 		float x = scanner.nextFloat(); // Devuelve un valor de tipo float 
 		
-		scanner.close(); // Cierra el objeto scanner para 
+		scanner.close(); // Cierra el ingreso de datos mediante la consola 
 		
-		System.out.println("\nEl número ingresado es " + x);
+		float numerador = (float)(2 * Math.pow(x, -3) + 5 * Math.pow(x, 2) - 8 * x);
+		float denominador = (float)Math.sqrt(9 * Math.pow(x, 3) - 2 * Math.pow(x, 2) + x);
+		
+		float resultado = numerador / denominador;
+		
+		System.out.print("El resultado es " + resultado);
+		
+		if(resultado > - 1 && resultado < 1) {
+			System.out.println(" y pertenece al rango <-1, 1>.");
+			
+		} else {
+			System.out.println(" y no pertenece al rango <-1, 1>.");
+		}
 	}
 }
